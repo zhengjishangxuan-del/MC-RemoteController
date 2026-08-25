e2dd1ba307224fb1d232ae5eac396f2245ea4f88
zhengjishangxuan-del <zhengjishangxuan@qq.com>
2026-08-26 06:59:43 +0800

    Update Windows server: Properties/AssemblyInfo.cs

---
+++ WindowsServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MC Remote Controller")]
+[assembly: AssemblyDescription("我的世界局域网远程控制系统 - Windows服务端")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MCRemoteController")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
